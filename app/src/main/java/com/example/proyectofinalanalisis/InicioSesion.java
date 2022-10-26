3f4066a6cdc8192445d54bb669dbd4b6ebaaf1ad
Jan Carlo De León <janleoncif11@gmail.com>
2022-10-25 18:58:03 -0600

    La etiqueta Registrar redirige a la interfaz de Registro

--- app/src/main/java/com/example/proyectofinalanalisis/InicioSesion.java
+++ app/src/main/java/com/example/proyectofinalanalisis/InicioSesion.java
@@ -1,7 +1,11 @@
 package com.example.proyectofinalanalisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class InicioSesion extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciosesion);
+
+
+
+    }
+
+    public void irARegistro(View v){
+        TextView info = findViewById(R.id.textViewRegistrarse);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Registro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
