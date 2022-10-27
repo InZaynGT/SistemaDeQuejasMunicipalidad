0ece52ecf13cf25eb1bde60c9511fbdb5f1b068c
InZaynGT <carlosperez280202@gmail.com>
2022-10-26 21:25:39 -0600

    interfaces 3 y 4

--- app/src/main/java/com/example/proyectofinalanalisis/QuejasUsuario.java
+++ app/src/main/java/com/example/proyectofinalanalisis/QuejasUsuario.java
@@ -1,7 +1,10 @@
 package com.example.proyectofinalanalisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class QuejasUsuario extends AppCompatActivity {
 
@@ -10,4 +13,27 @@ public class QuejasUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quejas_usuario);
     }
+
+    public void nuevaQueja(View v){
+        TextView info = findViewById(R.id.buttonNuevaQueja);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), creacionqueja.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    public void Salir(View v){
+        TextView info = findViewById(R.id.ButtonSalir);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), InicioSesion.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/proyectofinalanalisis/creacionqueja.java
+++ app/src/main/java/com/example/proyectofinalanalisis/creacionqueja.java
@@ -1,7 +1,10 @@
 package com.example.proyectofinalanalisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class creacionqueja extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class creacionqueja extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creacionqueja);
+    }
+
+    public void regresarQuejas(View v){
+        TextView info = findViewById(R.id.ButtonRegresar);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), QuejasUsuario.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
